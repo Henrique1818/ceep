e5e3648e68e54cf62756fdc47eae7637d3d39d8e
Henrique1818 <luiz.henrique01827@gmail.com>
2021-07-18 23:10:09 -0300

    add estilos no card nota

---
+++ src/components/CardNota/cardNota.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./estilos.css";
+
+export default class CardNota extends Component {
+  render() {
+    return (
+      <section className="card-nota">
+        <header>
+          <h3>Título</h3>
+        </header>
+        <p>Escreva sua nota</p>
+      </section>
+    );
+  }
+}
